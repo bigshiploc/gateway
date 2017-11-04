f907d38e3f4a0aba8ba8ab1ff02f3c4609539d56
liuchaoxue <482739566@qq.com>
2017-11-04 18:49:03 +0800

    子进程

--- services/api.js
+++ services/api.js
@@ -1,14 +1,13 @@
 const _ = require('underscore')
 const shortid = require('shortid')
 var spawn = require('child_process').spawn
-
 var passport = require('passport');
+
 const path = require('path')
 
 var fs = require("fs");
 var rp = require('request-promise');
 rp = rp.defaults({json: true});
-var isSave = false;
 const Faye = require('faye')
 const config = require('../config')
 var client = new Faye.Client(config.SERVERS.MQ_SERVER)
@@ -92,105 +91,6 @@ function getWrapperConfig() {
     })
 }
 
-var elasticsearch = require('elasticsearch');
-var esclient = new elasticsearch.Client({
-    host: config.SERVERS.ES_SERVER,
-});
-
-var child_process = require('child_process');
-
-var stationHistoryInfo = [];
-var labelHistoryInfo = [];
-var stationNum = 0;
-var stationLastOne = 0;
-var labelNum = 0;
-var labelLastOne = 0;
-
-var allDataObj = {};
-var lastDataArr = [];
-
-function getTimeDifference(start, end, allDataObj, fileName,res) {
-    var startTime = getTime(start);
-    var endTime = getTime(end);
-    var timeDifference = endTime - startTime;
-    console.log(timeDifference);
-    getLastData(startTime, endTime, timeDifference, allDataObj, fileName,res)
-}
-
-
-function getLastData(startTime, endTime, timeDifference, allDataObj, fileName,res) {
-    console.log('=====================time');
-    if (Object.keys(allDataObj).length == 0) {
-         fs.writeFileSync(path.join(__dirname, '../public/data/' + fileName), '[]')
-        isSave =true
-        return res.sendFile(path.join(__dirname, '../public/data/' + fileName))
-    }
-    for (var i = 0; i < timeDifference; i++) {
-        var obj = {};
-        for (var j in allDataObj) {
-            if (j in obj == true) {
-                getOneSecondData(obj, j, i, startTime, endTime, allDataObj);
-            } else {
-                obj[j] = [];
-                getOneSecondData(obj, j, i, startTime, endTime, allDataObj);
-            }
-        }
-        lastDataArr.push(obj);
-    }
-    saveDataArr(fileName,res);
-    // console.log(lastDataArr[0]);   // 这里得到基本的数据。（不包含节点信息的数据。）
-}
-
-function getOneSecondData(obj, name, i, startTime, endTime, allDataObj) {
-    for (var z = 0; z < allDataObj[name].length; z++) {
-        var timeData = getTime(allDataObj[name][z].timestamp);
-        if (startTime + i <= timeData && timeData < startTime + i + 1 && startTime <= timeData && timeData <= endTime) {
-            obj.timestamp = allDataObj[name][z].timestamp;
-            obj[name].push(allDataObj[name][z])
-        }
-    }
-}
-
-function saveDataArr(fileName,res) {
-    fs.writeFile(path.join(__dirname, '../public/data/' + fileName), JSON.stringify(lastDataArr), function (err) {
-        res.sendFile(path.join(__dirname, '../public/data/' + fileName))
-        isSave =true
-        console.log('--保存文件结束！！！')
-        lastDataArr.length = 0;
-    });
-}
-
-function getTime(timeStamp) {
-    return parseInt(timeStamp / 1000)
-}
-
-function getHistoryInfo(start, end, fileName,res) {
-    esclient.search({
-        index: 'bigship',
-        type: 'constituencies',
-        body: {
-            query: {
-                range: {timestamp: {gte: start, lte: end}}
-            }, size: 10000
-        }
-    }).then(function (resp) {
-        var data = resp.hits.hits;
-        var allDataObj = {};
-        for (var j = 0; j < data.length; j++) {
-            if (!allDataObj.hasOwnProperty(data[j]._source.eventname)) {
-                allDataObj[data[j]._source.eventname] = [];
-            }
-        }
-        for (var i = 0; i < data.length; i++) {
-            allDataObj[data[i]._source.eventname].push(data[i]._source)
-        }
-
-        getTimeDifference(start, end, allDataObj, fileName,res)
-    }, function (err) {
-        console.trace(err.message);
-    });
-}
-
 module.exports = function (app) {
     app.post('/login', function (req, res, next) {
         passport.authenticate('local', function (err, user) {
@@ -237,17 +137,27 @@ module.exports = function (app) {
         var timeDifference = getTime(req.query.endTime) - getTime(req.query.startTime);
         console.log(timeDifference);
         var fileName = new Date().getTime() + '.json';
-        try {
-            getHistoryInfo(req.query.startTime, req.query.endTime, fileName,res);
-        } catch (err) {
-            getHistoryInfo(req.query.startTime, req.query.endTime, fileName,res);
-            // res.sendFile(path.join(__dirname, '../public/data/' + fileName))
-        }
-        setTimeout(function () {
-            if(!isSave){
-                res.send(false)
+
+        getHistoryInfo(req.query.startTime, req.query.endTime, fileName, res);
+        var fork = require('child_process').fork;
+        var child = fork('./saveFile.js');
+        child.on('message', function (msg) {
+            if(msg!='close'){
+                res.sendFile(path.join(__dirname, '../public/data/' + fileName))
+
             }
-        },1000*20)
+            child.send('close')
+            console.log('parent get message: ' + JSON.stringify(msg));
+        });
+        var data = {
+            start: req.query.startTime, end: req.query.endTime, fileName: fileName, res: res,message:""
+        }
+        child.send(data);
+        // setTimeout(function () {
+        //     if (!isSave) {
+        //         res.send(false)
+        //     }
+        // }, 1000 * 20)
     })
 
     // app.post('/nodes/wrapper', function (req, res) {
---
+++ services/saveFile.js
@@ -0,0 +1,110 @@
+/**
+ * Created by chenyu on 17/11/4.
+ */
+
+var elasticsearch = require('elasticsearch');
+var esclient = new elasticsearch.Client({
+    host: config.SERVERS.ES_SERVER,
+});
+
+var child_process = require('child_process');
+
+var stationHistoryInfo = [];
+var labelHistoryInfo = [];
+var stationNum = 0;
+var stationLastOne = 0;
+var labelNum = 0;
+var labelLastOne = 0;
+
+var allDataObj = {};
+var lastDataArr = [];
+
+function getTimeDifference(start, end, allDataObj, fileName, res) {
+    var startTime = getTime(start);
+    var endTime = getTime(end);
+    var timeDifference = endTime - startTime;
+    console.log(timeDifference);
+    getLastData(startTime, endTime, timeDifference, allDataObj, fileName, res)
+}
+
+
+function getLastData(startTime, endTime, timeDifference, allDataObj, fileName, res) {
+    console.log('=====================time');
+    if (Object.keys(allDataObj).length == 0) {
+        fs.writeFileSync(path.join(__dirname, '../public/data/' + fileName), '[]')
+        return precess.send(path.join(__dirname, '../public/data/' + fileName))
+    }
+    for (var i = 0; i < timeDifference; i++) {
+        var obj = {};
+        for (var j in allDataObj) {
+            if (j in obj == true) {
+                getOneSecondData(obj, j, i, startTime, endTime, allDataObj);
+            } else {
+                obj[j] = [];
+                getOneSecondData(obj, j, i, startTime, endTime, allDataObj);
+            }
+        }
+        lastDataArr.push(obj);
+    }
+    saveDataArr(fileName, res);
+    // console.log(lastDataArr[0]);   // 这里得到基本的数据。（不包含节点信息的数据。）
+}
+
+function getOneSecondData(obj, name, i, startTime, endTime, allDataObj) {
+    for (var z = 0; z < allDataObj[name].length; z++) {
+        var timeData = getTime(allDataObj[name][z].timestamp);
+        if (startTime + i <= timeData && timeData < startTime + i + 1 && startTime <= timeData && timeData <= endTime) {
+            obj.timestamp = allDataObj[name][z].timestamp;
+            obj[name].push(allDataObj[name][z])
+        }
+    }
+}
+
+function saveDataArr(fileName, res) {
+    fs.writeFile(path.join(__dirname, '../public/data/' + fileName), JSON.stringify(lastDataArr), function (err) {
+        // res.sendFile(path.join(__dirname, '../public/data/' + fileName))
+        precess.send(path.join(__dirname, '../public/data/' + fileName))
+        console.log('--保存文件结束！！！')
+        lastDataArr.length = 0;
+    });
+}
+
+function getTime(timeStamp) {
+    return parseInt(timeStamp / 1000)
+}
+
+function getHistoryInfo(start, end, fileName, res) {
+    esclient.search({
+        index: 'bigship',
+        type: 'constituencies',
+        body: {
+            query: {
+                range: {timestamp: {gte: start, lte: end}}
+            }, size: 10000
+        }
+    }).then(function (resp) {
+        var data = resp.hits.hits;
+        var allDataObj = {};
+        for (var j = 0; j < data.length; j++) {
+            if (!allDataObj.hasOwnProperty(data[j]._source.eventname)) {
+                allDataObj[data[j]._source.eventname] = [];
+            }
+        }
+        for (var i = 0; i < data.length; i++) {
+            allDataObj[data[i]._source.eventname].push(data[i]._source)
+        }
+
+        getTimeDifference(start, end, allDataObj, fileName, res)
+    }, function (err) {
+        console.trace(err.message);
+    });
+}
+process.on('message', function(data){
+    if (data.message == 'close') {
+        return process.exit()
+    }
+    getHistoryInfo(data.start, data.end, data.fileName, data.res)
+    setTimeout(function () {
+        process.send('close')
+    }, 1000 * 20)
+});

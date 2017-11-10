776ae9666de6f123c8039aa197fe362f2160799d
liuchaoxue <482739566@qq.com>
2017-11-10 15:05:18 +0800

    修改判断

--- services/api.js
+++ services/api.js
@@ -290,7 +290,7 @@ module.exports = function (app) {
     }
 
     function labelHistoryData(data, req, res) {
-        if ((labelHistoryInfo[labelLastOne] && labelHistoryInfo[labelLastOne].length == 0) ||
+        if (labelHistoryInfo[labelLastOne] && (labelHistoryInfo[labelLastOne].length == 0) ||
             (labelHistoryInfo[labelLastOne].length == 1 && labelHistoryInfo[labelLastOne][0].hasOwnProperty('delete'))) {
             getOneLabelData(data, req, res)
         } else if (labelLastOne <= labelHistoryInfo.length - 1) {
